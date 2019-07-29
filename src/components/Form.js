2c03849f335e4a111c9198d2cd1c0843ddcada4e
fr-me-jim <fr.me.jim@gmail.com>
2019-07-29 23:19:50 +0200

    added events context

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { CategoriesConsumer } from '../context/CategoriesContext';
+import { EventsConsumer } from '../context/EventsContext';
 
 class Form extends Component {
     state = { 
@@ -18,53 +19,65 @@ class Form extends Component {
 
     render() { 
         return ( 
-            <form>
-                <fieldset className="uk-fieldset uk-margin">
-                    <legend className="uk-legend uk-text-center">
-                        Search events by Name/City or Category
-                    </legend>
-                </fieldset>
+            <EventsConsumer>
+                {(value) => {
 
-                <div className="uk-column-1-3@m uk-margin">
-                    <div className="uk-margin" uk-margin="true" >
-                        <input 
-                            name="name"
-                            className="uk-input"
-                            placeholder="Name or City of the Event"
-                            type="text"
-                            onChange={this.handleGetDataEvent}
-                        />
-                    </div>
-
-                    <div className="uk-margin" uk-margin="true" >
-                        <select 
-                            name="category" 
-                            className="uk-select"
-                            onChange={this.handleGetDataEvent}
+                    return (
+                        <form
+                            onSubmit={e => {
+                                e.preventDefault();
+                                value.handleGetEvents(this.state);
+                            }}
                         >
-                            <option value="">--Select Category--</option>
-                            <CategoriesConsumer>
-                                {(value) => {
-                                    return (
-                                        value.categories.map(category => (
-                                            <option key={category.id}  value={category.id} 
-                                                data-uk-form-select >
-                                                    {category.name}
-                                            </option>
-                                        ) )
-                                    );
-                                }}
-                            </CategoriesConsumer>
-                            
-                        </select>
-                    </div>
+                            <fieldset className="uk-fieldset uk-margin">
+                                <legend className="uk-legend uk-text-center">
+                                    Search events by Name/City or Category
+                                </legend>
+                            </fieldset>
 
-                    <div>
-                        <input type="submit" className="uk-button uk-button-danger"
-                            value="Search Events" />
-                    </div>
-                </div>                
-            </form>    
+                            <div className="uk-column-1-3@m uk-margin">
+                                <div className="uk-margin" uk-margin="true" >
+                                    <input 
+                                        name="name"
+                                        className="uk-input"
+                                        placeholder="Name or City of the Event"
+                                        type="text"
+                                        onChange={this.handleGetDataEvent}
+                                    />
+                                </div>
+
+                                <div className="uk-margin" uk-margin="true" >
+                                    <select 
+                                        name="category" 
+                                        className="uk-select"
+                                        onChange={this.handleGetDataEvent}
+                                    >
+                                        <option value="">--Select Category--</option>
+                                        <CategoriesConsumer>
+                                            {(value) => {
+                                                return (
+                                                    value.categories.map(category => (
+                                                        <option key={category.id}  value={category.id} 
+                                                            data-uk-form-select >
+                                                                {category.name}
+                                                        </option>
+                                                    ) )
+                                                );
+                                            }}
+                                        </CategoriesConsumer>
+                                        
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <input type="submit" className="uk-button uk-button-danger"
+                                        value="Search Events" />
+                                </div>
+                            </div>                
+                        </form> 
+                    );
+                }}  
+            </EventsConsumer> 
         );
     }
 }
